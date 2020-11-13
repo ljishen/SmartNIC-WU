70c863d13817e9877ca88306dd67d004d4d5a620
Jianshen Liu <jliu120@ucsc.edu>
2020-11-13 16:47:31 -0700

    Support testing with delays and add descriptions of steps for the benchmark experiment

--- scripts/pktgen_bench/bench.sh
+++ scripts/pktgen_bench/bench.sh
@@ -8,12 +8,34 @@ IFNAME=ens1f1
 PKT_SIZE=60 # in bytes
 RUNTIME=20  # in seconds
 
-ARR_CLONE_SKB=({0..25..5})
-ARR_BURST=(1 {5..25..5})
-ARR_THREADS=({1..10})
-
 ROUNDS_PER_TEST=3
 OUTPUT_DIR="./results"
+
+# The first step of testing is to evaluate the least value of
+# THREADS, CLONE_SKB, and BURST that can achieve the maximum
+# throughput of the network device.
+# Once the three numbers are identified, we can evaluate how
+# various delays added between packets would deviate the throughput
+# from the maximum capacity. We perform this step by fixing the
+# value of CLONE_SKB and BURST identified in the first step, but
+# varying the value of DELAY and THREADS. Increasing threads in the
+# test can evaluate whether or how much the throughput can be
+# improved under a given packet delay.
+#
+# ===== STEP ONE =====
+ARR_DELAY=(0)
+ARR_THREADS=({1..10})
+ARR_CLONE_SKB=({0..25..5})
+ARR_BURST=(1 {5..25..5})
+# ===================
+#
+# ===== STEP TWO =====
+# exmple delays for evaluation
+# ARR_DELAY=(0 125 250 500 1000 2000 4000 8000 16000)
+# ARR_THREADS=({1..10})
+# ARR_CLONE_SKB=(20)  # update this value
+# ARR_BURST=(20)      # update this value
+# ====================
 # --------------------------
 
 if ! [[ -d /sys/class/net/"$IFNAME" ]]; then
@@ -27,15 +49,17 @@ if [[ "$operstate" != "up" ]]; then
   exit 1
 fi
 
+OUTPUT_FILE="${OUTPUT_DIR}/pkt_size_${PKT_SIZE}bytes.data"
+if [[ -f "$OUTPUT_FILE" ]]; then
+  echo "[ERROR] we don't want to overwrite the existing file $OUTPUT_FILE." >&2
+  exit 2
+else
+  mkdir -p "$OUTPUT_DIR"
+fi
+
 function get_date() {
   date --iso-8601=seconds
 }
-
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-OUTPUT_FILE="${OUTPUT_DIR}/pkt_size_${PKT_SIZE}bytes.data"
-
-mkdir -p "$OUTPUT_DIR"
 
 got_device_info=false
 
@@ -78,52 +102,59 @@ fi
   printf '# Start of test: %s\n\n' "$(get_date)"
 } >> "$OUTPUT_FILE"
 
-printf 'THREADS\tCLONE_SKB\tBURST\tTHROUGHPUT (Mb/sec)\tSTD\n' >> "$OUTPUT_FILE"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-for t in "${ARR_THREADS[@]}"; do
-  for c in "${ARR_CLONE_SKB[@]}"; do
-    for b in "${ARR_BURST[@]}"; do
-      echo "[$(get_date)][INFO] running" \
-           "THREADS=$t (${ARR_THREADS[0]}..${ARR_THREADS[-1]})," \
-           "CLONE_SKB=$c (${ARR_CLONE_SKB[0]}..${ARR_CLONE_SKB[-1]})," \
-           "BURST=$b (${ARR_BURST[0]}..${ARR_BURST[-1]})"
+printf 'DELAY (ns)\tTHREADS\tCLONE_SKB\tBURST\tTHROUGHPUT (Mb/sec)\tSTD\n' >> "$OUTPUT_FILE"
 
-      records=()
-      for _ in $(seq "$ROUNDS_PER_TEST"); do
-        res=$(
-          "$SCRIPT_DIR"/xmit_multiqueue.sh \
-            -i "$IFNAME" \
-            -e "$RUNTIME" \
-            -s "$PKT_SIZE" \
-            -t "$t" \
-            -c "$c" \
-            -b "$b" 2>&1 \
-              | grep -oP "\d+(?=Mb/sec)" \
-              | awk '{ sum += $1 } END { print sum }'
-          )
-        records+=("$res")
+for d in "${ARR_DELAY[@]}"; do
+  for t in "${ARR_THREADS[@]}"; do
+    for c in "${ARR_CLONE_SKB[@]}"; do
+      for b in "${ARR_BURST[@]}"; do
+        echo "[$(get_date)][INFO] running" \
+             "DELAY=$d (${ARR_DELAY[0]}..${ARR_DELAY[-1]})," \
+             "THREADS=$t (${ARR_THREADS[0]}..${ARR_THREADS[-1]})," \
+             "CLONE_SKB=$c (${ARR_CLONE_SKB[0]}..${ARR_CLONE_SKB[-1]})," \
+             "BURST=$b (${ARR_BURST[0]}..${ARR_BURST[-1]})"
+
+        records=()
+        for _ in $(seq "$ROUNDS_PER_TEST"); do
+          res=$(
+            "$SCRIPT_DIR"/xmit_multiqueue.sh \
+              -i "$IFNAME" \
+              -e "$RUNTIME" \
+              -s "$PKT_SIZE" \
+              -l "$d" \
+              -t "$t" \
+              -c "$c" \
+              -b "$b" 2>&1 \
+                | grep -oP "\d+(?=Mb/sec)" \
+                | awk '{ sum += $1 } END { print sum }'
+            )
+          records+=("$res")
+        done
+
+        printf '%s\n' "${records[@]}" | awk \
+          -v d="$d" \
+          -v t="$t" \
+          -v c="$c" \
+          -v b="$b" \
+          '
+            {
+              sum += $0
+              sum_sq += $0 ^ 2
+            }
+
+            END {
+              printf("%d\t%d\t%d\t%d\t%.2f\t%.2f\n",
+                d, t, c, b,
+                sum / NR,
+                sqrt(sum_sq / NR - (sum / NR) ^ 2))
+            }
+          ' >> "$OUTPUT_FILE"
       done
-
-      printf '%s\n' "${records[@]}" | awk \
-        -v t="$t" \
-        -v c="$c" \
-        -v b="$b" \
-        '
-          {
-            sum += $0
-            sum_sq += $0 ^ 2
-          }
-
-          END {
-            printf("%d\t%d\t%d\t%.2f\t%.2f\n",
-              t, c, b,
-              sum / NR,
-              sqrt(sum_sq / NR - (sum / NR) ^ 2))
-          }
-        ' >> "$OUTPUT_FILE"
     done
   done
 done
 
-printf '\n# End of test: %s' "$(get_date)"
+printf '\n# End of test: %s' "$(get_date)" >> "$OUTPUT_FILE"
 echo "[$(get_date)][INFO] completed!"
